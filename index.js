188c5868ac6baa46259e56a156fb716893867adc
HTML-09 <knockbackmc125@gmail.com>
2025-01-18 10:10:25 +0300

    First launch
    this is my website inspired by me HTML

---
+++ index.js
@@ -0,0 +1,53 @@
+/*document.getElementById("goat").onclick=function(){
+    alert("you have been scarmed")
+}*/
+
+//alert("")
+
+//prompt("")
+
+/*document.getElementById("goat").onclick=function(){
+    document.getElementById("goat").innerHTML="i am intelligent"
+}*/
+
+/*document.getElementById("goat").onclick=function(){
+    alert('on 0707537787 or knockbackmc125@gmail.com')
+}
+
+document.getElementById("gone").onclick=function(){
+
+    alert("we are an amazing store on the world market. ")
+}*/
+
+
+
+
+/*document.getElementById("gone").addEventListener("click",function(){
+    document.getElementById("gone").style.background="red"
+    
+document.getElementById("gone").style.width="150px"  
+})
+
+if(navigator.onLine){
+    alert("online")
+}*/
+
+
+
+document.getElementById("gone").onclick=function(){
+
+location.href="fool.html"
+}
+
+
+
+document.getElementById("goat").onclick=function(){
+
+    location.href="lol.html"
+}
+
+
+document.getElementById("node").onclick=function(){
+
+    location.href="lamao.html"
+}
